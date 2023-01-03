b6306a68223fe87d53cd987865247e2c4a5d2dbc
Vinicius Maciel <vm.viniciusmaciel@gmail.com>
2023-01-02 23:43:46 -0300

    feat: css reset and basic user styles

---
+++ src/assets/css/userStyles.jsx
@@ -0,0 +1,32 @@
+import { createGlobalStyle } from "styled-components";
+
+const UserStyles = createGlobalStyle`
+    * {
+      box-sizing: border-box;
+      /* outline: 1px dashed palevioletred; */
+    }
+    body {
+      font-family: 'Lato', sans-serif;
+      background: #262835;
+      color: #fff;
+      padding-bottom: 80px;
+    }
+    a {
+      text-decoration: none;
+ 
+      
+      &:hover{
+        cursor: pointer;
+      }
+    }
+    button {
+      &:hover{
+        cursor: pointer;
+      }
+    }
+    ul {
+      list-style: none;
+    }
+`;
+
+export default UserStyles;
