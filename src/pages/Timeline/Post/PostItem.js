f2d40d75fbab3d4b4b3c706ef8d4372a3f71f24d
Alysson Marley Dias <callistomain@gmail.com>
2023-01-04 03:34:55 -0300

    feat: post edit button and functionality

--- src/pages/Timeline/Post/PostItem.js
+++ src/pages/Timeline/Post/PostItem.js
@@ -1,15 +1,78 @@
+import { useEffect, useRef, useState } from "react";
+import { updatePost } from "../../../services/api";
 import PostLink from "./Link/PostLink";
 import { PostStyle } from "./PostStyle";
+import { RiPencilFill } from 'react-icons/ri';
+import { IconContext } from "react-icons";
 
 export default function PostItem({data}) {
-  const {image_url, name, link, message} = data;
+  const {id, image_url, name, link, message, owner} = data;
+  const [text, setText] = useState(message);
+  const [currMessage, setCurrMessage] = useState(message);
+  const [editing, setEditing] = useState(false);
+  // eslint-disable-next-line
+  const [loading, setLoading] = useState(false);
+  const inputRef = useRef(null);
+
+  function toogleEdit() {
+    setEditing(editing => {
+      if (editing) setText(currMessage);
+      return !editing;
+    });
+  }
+
+  useEffect(() => {
+    if (editing) {
+      inputRef.current.focus();
+      inputRef.current.selectionStart = inputRef.current.value.length;
+    }
+  }, [editing]);
+
+  function handleText(e) {
+    const ref = e.target;
+    setText(ref.value);
+    ref.style.height = 0;
+    ref.style.height = (ref.scrollHeight) + "px";
+  }
+
+  function keyPressHandler(e) {
+    if (e.key === "Escape") toogleEdit();
+    if (e.key === "Enter") {
+      e.target.disabled = true;
+      setLoading(true);
+
+      updatePost({id, message: text})
+      .then(() => {
+        setLoading(false);
+        setEditing(false);
+        setCurrMessage(text);
+      })
+      .catch(err => {
+        setLoading(false);
+        e.target.disabled = false;
+        window.alert("Não foi possível salvar as alterações.");
+        console.log(err);
+      });
+    }
+  }
 
   return (
     <PostStyle>
+      {owner && <div className="options">
+        <IconContext.Provider value={{ size:"20px" }}>
+          <div className="edit" onClick={toogleEdit}><RiPencilFill/></div>
+        </IconContext.Provider>
+      </div>}
       <img src={image_url} alt=""/>
       <div className="content">
         <div className="name">{name}</div>
-        <div className="message">{message}</div>
+        {editing 
+          ? <textarea type="text" name="editmsg" id="editmsg" maxLength="1000" 
+              value={text} onFocus={handleText} onChange={handleText} ref={inputRef}
+              onKeyDown={keyPressHandler}
+            />
+          : <div className="message">{currMessage}</div>
+        }
         {link ? <PostLink data={link}/> : null}
       </div>
     </PostStyle>
--- src/pages/Timeline/Publish/PublishItem.js
+++ src/pages/Timeline/Publish/PublishItem.js
@@ -1,11 +1,11 @@
-import axios from "axios";
 import { useState } from "react";
+import { postPost } from "../../../services/api";
 import { PublishStyle } from "./PublishStyle";
 
 export default function PublishItem({image, updateTimeline}) {
   const [loading, setLoading] = useState(false);
 
-  function eventHandler(e) {
+  function formHandler(e) {
     const items = Array.from(e.target);
     e.preventDefault();
     setLoading(true);
@@ -15,9 +15,7 @@ export default function PublishItem({image, updateTimeline}) {
     const data = { link: link.value };
     if (message.value) data.message = message.value;
 
-    const token = "one";
-    const headers = {Authorization: "Bearer " + token};
-    axios.post("http://localhost:5000/posts", data, {headers})
+    postPost(data)
     .then(r => {
       setLoading(false);
       updateTimeline();
@@ -37,7 +35,7 @@ export default function PublishItem({image, updateTimeline}) {
   return (
     <PublishStyle className="publish">
       <img src={image ? image : "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7c/User_font_awesome.svg/2048px-User_font_awesome.svg.png"} alt=""/>
-      <form className="content" onSubmit={eventHandler}>
+      <form className="content" onSubmit={formHandler}>
         <div className="title">What are you going to share today?</div>
         <input type="url" name="link" id="link" placeholder="http://..." required/>
         <textarea type="text" name="message" id="message" rows="3" maxLength="1000" placeholder="Awesome article about #javascript"/>
