9fe15bd4a4f8153bf59ea1031da937dd9ef79932
Alysson Marley Dias <callistomain@gmail.com>
2023-01-06 21:17:11 -0300

    refact: api user update

--- src/services/api.js
+++ src/services/api.js
@@ -5,53 +5,45 @@ const URL = "https://linkr-api-itfc.onrender.com/";
 
 export const api = axios.create({ baseURL: URL });
 
-let TOKEN = JSON.parse(localStorage.getItem("user"))?.token;
+const TOKEN = JSON.parse(localStorage.getItem("user"))?.token;
 api.defaults.headers["Authorization"] = `Bearer ${TOKEN}`;
 
-function updateUser () {
-  TOKEN = JSON.parse(localStorage.getItem("user"))?.token;
+export function updateUser () {
+  const TOKEN = JSON.parse(localStorage.getItem("user"))?.token;
   api.defaults.headers["Authorization"] = `Bearer ${TOKEN}`;
 }
 
 export function postSignUp(body) {
-  updateUser();
   return api.post("/sign-up", body);
 }
 
 export function signIn(body) {
-  updateUser();
   return api.post("/sign-in", body);
 }
 
 export function logOut() {
-  updateUser();
   return api.delete("/users");
 }
 
 // POSTS ==========================
 export function postPost(body) {
-  updateUser();
   return api.post("/posts", body);
 }
 
 export function getPosts(hashtag=null) {
-  updateUser();
   const route = hashtag? `/posts?hashtag=${hashtag}`: `/posts`;
   return api.get(route);
 }
 
 export function updatePost(body) {
-  updateUser();
   return api.patch("/posts", body);
 }
 
 export function deletePost(id) {
-  updateUser();
   return api.delete("/posts/" + id);
 }
 
 // HASHTAGS ========================== 
 export function getTrendingHashtags(){
-  updateUser();
   return api.get("/hashtags")
 }
